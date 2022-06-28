4c92c61be085066acb14371e30df1d3768c0967f
tewarig <gauravtewari111@gmail.com>
2022-06-28 23:50:51 +0530

    feat: protected route added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import logo from "./logo.svg";
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "./comp/navBar/navBar";
+import DashBoard from "./pages/dashboard/dashboard";
+import Home from "./pages/home/home";
+import Poll from "./pages/poll/poll";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./comp/protectedRoute/protectedRoute";
 
 function App() {
   return (
     <ChakraProvider>
       <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" index element={<Home />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <DashBoard />
+              </ProtectedRoute>
+            }
+          ></Route>
+          <Route path="/poll" element={<Poll />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
---
+++ src/comp/protectedRoute/protectedRoute.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import { auth } from "../../module/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!loading && !user) {
+      navigate("/");
+    }
+  }, [user, loading, error]);
+
+  if (loading) {
+    return <></>;
+  }
+  if (user) return <div>{children}</div>;
+}
+
+export default ProtectedRoute;
