e1c680eab49d7a855f48a0e93fdc3c2b66b60453
tewarig <gauravtewari111@gmail.com>
2022-06-30 13:23:27 +0530

    chore: poll ui added

--- src/pages/poll/poll.js
+++ src/pages/poll/poll.js
@@ -1,5 +1,29 @@
 import React from "react";
+import {
+  Box,
+  Heading,
+  Progress,
+  Flex,
+  Button,
+  Input,
+  IconButton,
+} from "@chakra-ui/react";
 
 export default function Poll() {
-  return <div>Poll</div>;
+  return (
+    <Box alignItems={"center"}>
+      <Box
+        backgroundColor="#f2f2f2"
+        borderRadius={"10"}
+        mt="5"
+        width={"80%"}
+        padding="10"
+        alignItems={"center"}
+        textAlign="center"
+        alignSelf={"center"}
+        ml="150"
+        boxShadow="xs"
+      ></Box>
+    </Box>
+  );
 }
